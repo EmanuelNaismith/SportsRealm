85e2b29fd85aa5d4e14114db85b1de67d91d1648
EmanuelNaismith <emanuel.alexandi@d-fine.de>
2023-01-23 14:05:29 +0100

    test commit

---
+++ sportsrealm-backend/src/main/kotlin/org/sportsrealm/sportsrealmbackend/SportsrealmBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.sportsrealm.sportsrealmbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportsrealmBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SportsrealmBackendApplication>(*args)
+}
---
+++ sportsrealm-backend/src/test/kotlin/org/sportsrealm/sportsrealmbackend/SportsrealmBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sportsrealm.sportsrealmbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportsrealmBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
